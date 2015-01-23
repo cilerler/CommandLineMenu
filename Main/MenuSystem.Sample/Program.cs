26413664760a103232585cb4326b2369ff612d9a
cilerler_cp <SND\cilerler_cp>
2015-01-23 23:03:51 +0000

    Transfer

---
+++ Main/MenuSystem.Sample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MenuSystem.Sample
+{
+    class Program
+    {
+        /// <code>
+        /// static void Main()
+        /// {
+        ///     var menu = new MenuSystem.MenuDrivenApplication("Hello Menu");
+        ///     menu.Add(HelloWorld);
+        ///     menu.Run();
+        /// }
+        ///
+        /// static void HelloWorld()
+        /// {
+        ///     Console.WriteLine("Hello");
+        /// }
+        /// </code>
+        static void Main(string[] args)
+        {
+            var menu = new MenuSystem.MenuDrivenApplication("Hello Menu");
+            menu.Add(HelloWorld);
+            menu.Run();
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
